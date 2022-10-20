7af0690386ea178875e0ececfdc734dc425e98b4
Cubbon <alan.cubbon@crownagentsbank.com>
2022-10-20 14:29:13 +0100

    Addition of as.Date when passing input parameter

--- run-bm-calcs.R
+++ run-bm-calcs.R
@@ -1,14 +1,16 @@
 library(caim)
 library(lubridate)
 
+setwd('H:/R/caim-bm-calcs')
+
 config <- fread(ifelse(file.exists("./config/config.csv"), "./config/config.csv", "./config/config_default.csv"))
 prefs <- list()
 for (x in 1:nrow(config)) {
   prefs[[config[x, parameter]]] <- config[x, value]
 }
 
-WORKING_DIR <- prefs$working_dir
-setwd(WORKING_DIR)
+#WORKING_DIR <- prefs$working_dir
+#setwd(WORKING_DIR)
 
 dt_1d_ago <- Sys.Date() - 1
 if (wday(dt_1d_ago) == 1){
@@ -28,8 +30,8 @@ dt_list <- c(dt_2d_ago, dt_1d_ago)
 
 for (dt_eff in dt_list) {
   print(format(as.Date(dt_eff),"%Y-%m-%d"))
-  
+
   xfun::Rscript_call(
     rmarkdown::render,
-    list(input = 'caim-bm-calcs.Rmd', params = list(dt_eff = dt_eff)))
+    list(input = 'caim-bm-calcs.Rmd', params = list(dt_eff = as.Date(dt_eff))))
 }
